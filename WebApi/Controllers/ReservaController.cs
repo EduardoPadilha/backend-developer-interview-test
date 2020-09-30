642e24b1756e0ba32eca5b5ee4ee65d81a8e8bb8
Eduardo Padilha <todmalkavia@gmail.com>
2020-09-30 16:39:14 -0300

    Criação do dockerfile

--- WebApi/Controllers/ReservaController.cs
+++ WebApi/Controllers/ReservaController.cs
@@ -3,11 +3,7 @@ using FeriasCo.Cortex.Interfaces;
 using FeriasCo.Cortex.Servicos;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Server.IIS;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace FeriasCo.WebApi.Controllers
 {
